67ae9ac694968df79f68b1c884b627a28e745868
HemantBadhala <hemantbadhala24@gmail.com>
2026-09-21 01:44:41 +0530

    added user register API

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello! Mera Printing Marketplace server chal raha hai!');
 });
@@ -11,6 +13,28 @@ app.get('/test-db', async (req, res) => {
   try {
     const result = await pool.query('SELECT NOW()');
     res.send(`Database connected! Time: ${result.rows[0].now}`);
+  } catch (err) {
+    res.send(`Error: ${err.message}`);
+  }
+});
+
+app.get('/users', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM users');
+    res.json(result.rows);
+  } catch (err) {
+    res.send(`Error: ${err.message}`);
+  }
+});
+
+app.post('/users', async (req, res) => {
+  try {
+    const { name, phone_number, email, role } = req.body;
+    const result = await pool.query(
+      'INSERT INTO users (name, phone_number, email, role) VALUES ($1, $2, $3, $4) RETURNING *',
+      [name, phone_number, email, role]
+    );
+    res.json(result.rows[0]);
   } catch (err) {
     res.send(`Error: ${err.message}`);
   }
