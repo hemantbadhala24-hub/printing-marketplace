a4288b082b8144bc49a7c248a920178779fc7a90
HemantBadhala <hemantbadhala24@gmail.com>
2026-09-21 00:57:44 +0530

    connected postgresql database

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+const pool = require('./db');
 const express = require('express');
 const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello! Mera Printing Marketplace server chal raha hai!');
+});
+
+app.get('/test-db', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT NOW()');
+    res.send(`Database connected! Time: ${result.rows[0].now}`);
+  } catch (err) {
+    res.send(`Error: ${err.message}`);
+  }
 });
 
 app.listen(PORT, () => {
