c1d8f49b9ba5fa4dcf24ab9d8478b4a33ed75dda
HemantBadhala <hemantbadhala24@gmail.com>
2026-09-21 13:15:17 +0530

    added registration form

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json());
-
+app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.send('Hello! Mera Printing Marketplace server chal raha hai!');
 });
